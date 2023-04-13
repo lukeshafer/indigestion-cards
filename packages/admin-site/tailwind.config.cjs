341e559f8f744f5438362b243e18053b7b751e65
Luke <hello@lukeshafer.com>
2023-04-13 19:08:53 -0400

    Add fonts and a few basic styles

--- packages/admin-site/tailwind.config.cjs
+++ packages/admin-site/tailwind.config.cjs
@@ -1,8 +1,21 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				display: ['Rabbid Highway Sign II', ...fontFamily.sans],
+				heading: ['Mont', ...fontFamily.sans],
+			},
+			colors: {
+				brand: {
+					main: '#fd94e6',
+					secondary: '#f66efa',
+				},
+			},
+		},
 	},
 	plugins: [],
 }
