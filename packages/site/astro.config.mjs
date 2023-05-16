96a7386464cb427610bd7034ec9a54be24db6690
Luke <hello@lukeshafer.com>
2023-05-15 22:58:05 -0400

    Add card design stats overview, update breakdown to table view. Closes #41.

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -2,23 +2,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import aws from 'astro-sst/lambda';
-
 import prefetch from '@astrojs/prefetch';
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: aws(),
-	integrations: [tailwind()], //, prefetch()],
-	experimental: {
-		middleware: true,
-	},
-	vite: {
-		ssr: {
-			external: ['electrodb'],
-		},
-		optimizeDeps: {
-			exclude: ['sst'],
-		},
-	},
+  output: 'server',
+  adapter: aws(),
+  integrations: [tailwind(), alpinejs()],
+  //, prefetch()],
+  experimental: {
+    middleware: true
+  },
+  vite: {
+    ssr: {
+      external: ['electrodb']
+    },
+    optimizeDeps: {
+      exclude: ['sst']
+    }
+  }
 });
