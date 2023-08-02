952357e2c0d354c688dc6f8d02122d3b0c96d3fc
Luke Shafer <lukealanshafer@gmail.com>
2023-08-02 12:25:32 -0400

    Remove unused div

--- packages/site/src/components/cards/Card.tsx
+++ packages/site/src/components/cards/Card.tsx
@@ -23,41 +23,39 @@ export default function Card(props: Props) {
 	const cardDescription = () => (isFullArt() ? '' : props.cardDescription);
 
 	return (
-		<div>
-			<TiltCardEffect shiny={true}>
-				<article
-					class="card-wrapper card-aspect-ratio relative w-72 bg-cover text-left"
+		<TiltCardEffect shiny={true}>
+			<article
+				class="card-wrapper card-aspect-ratio relative w-72 bg-cover text-left"
+				style={{
+					scale: props.scale,
+					'background-color': props.rarityColor,
+					'view-transition-name': `card-${props.instanceId ?? props.designId}`,
+				}}>
+				<img src={props.imgUrl} alt={props.cardName} class="absolute inset-0" />
+				<img src={props.frameUrl} alt="" class="absolute inset-0" />
+				<h3 class="font-display absolute left-[12%] top-[4.9%] w-[66%] text-[0.9rem] font-bold italic text-slate-900">
+					{cardName()}
+				</h3>
+				<p
 					style={{
-						scale: props.scale,
-						'background-color': props.rarityColor,
-						'view-transition-name': `card-${props.instanceId ?? props.designId}`,
+						'--left': '11%',
+						top: '68.5%',
+						height: '15%',
+						width: 'calc(100% - var(--left) * 2)',
+						left: 'var(--left)',
+					}}
+					class="card-description font-heading absolute p-2 text-sm font-normal text-black">
+					{cardDescription()}
+				</p>
+				<p
+					class="font-display absolute bottom-2 right-4 text-xs font-bold italic"
+					classList={{
+						'text-black': !isFullArt(),
+						'text-white': isFullArt(),
 					}}>
-					<img src={props.imgUrl} alt={props.cardName} class="absolute inset-0" />
-					<img src={props.frameUrl} alt="" class="absolute inset-0" />
-					<h3 class="font-display absolute left-[12%] top-[4.9%] w-[66%] text-[0.9rem] font-bold italic text-slate-900">
-						{cardName()}
-					</h3>
-					<p
-						style={{
-							'--left': '11%',
-							top: '68.5%',
-							height: '15%',
-							width: 'calc(100% - var(--left) * 2)',
-							left: 'var(--left)',
-						}}
-						class="card-description font-heading absolute p-2 text-sm font-normal text-black">
-						{cardDescription()}
-					</p>
-					<p
-						class="font-display absolute bottom-2 right-4 text-xs font-bold italic"
-						classList={{
-							'text-black': !isFullArt(),
-							'text-white': isFullArt(),
-						}}>
-						{props.cardNumber} / {props.totalOfType}
-					</p>
-				</article>
-			</TiltCardEffect>
-		</div>
+					{props.cardNumber} / {props.totalOfType}
+				</p>
+			</article>
+		</TiltCardEffect>
 	);
 }
