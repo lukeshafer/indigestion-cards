5d1660ad76fb9bcb13e01c61df61eb3d0428e831
Luke Shafer <lukealanshafer@gmail.com>
2023-08-07 23:04:07 -0400

    Add card list sorting

---
+++ packages/site/src/components/cards/CardList.tsx
@@ -0,0 +1,105 @@
+import { routes } from '@/constants';
+import Card from '@/components/cards/Card';
+import { For, Show, createSignal } from 'solid-js';
+import { Dynamic } from 'solid-js/web';
+import styles from './CardList.module.css';
+import { Select } from '../form';
+import type { CardInstanceEntity } from '@lil-indigestion-cards/core/card';
+
+type CardType = Parameters<typeof Card>[0] & Partial<CardInstanceEntity>;
+
+const sortTypes = [
+	{ value: 'card-name-asc', label: 'Card Name (A-Z)' },
+	{ value: 'card-name-desc', label: 'Card Name (Z-A)' },
+	{ value: 'rarest', label: 'Most to Least Rare' },
+	{ value: 'common', label: 'Least to Most Rare' },
+] as const;
+
+type SortType = (typeof sortTypes)[number]['value'];
+
+export default function CardList(props: {
+	cards: CardType[];
+	showUsernames?: boolean;
+	sessionType?: string;
+	noSort?: boolean;
+	sortOnlyBy?: SortType[];
+}) {
+	const element = () => (props.sessionType === 'admin' ? 'a' : 'div');
+	const allowedSortTypes = () =>
+		props.sortOnlyBy?.length
+			? sortTypes.filter((type) => props.sortOnlyBy?.includes(type.value))
+			: sortTypes.slice()
+	const [sort, setSort] = createSignal<string>(allowedSortTypes()[0].value);
+
+	const sortedCards = () => sortCards({ cards: props.cards, sort: sort() });
+
+	return (
+		<div class="flex flex-col gap-3">
+			{props.noSort ? null : (
+				<div class="ml-auto flex w-fit">
+					<Select
+						name="sort"
+						label="Sort by"
+						setValue={(val) => document.startViewTransition(() => setSort(val))}
+						options={allowedSortTypes()}
+					/>
+				</div>
+			)}
+			<ul
+				class="grid w-full justify-center justify-items-center gap-x-2 gap-y-14 px-3 md:gap-x-6"
+				classList={{ [styles.cardList]: true }}>
+				<Show when={sortedCards().length > 0} fallback={<p>No cards found</p>}>
+					<For each={sortedCards()}>
+						{(card) => (
+							<div class="w-fit">
+								<Dynamic
+									component={element()}
+									rel="prefetch"
+									href={`${routes.INSTANCES}/${card.designId}/${
+										card.instanceId ?? ''
+									}`}>
+									<Card {...card} scale="var(--card-scale)" />
+								</Dynamic>
+								<Show when={props.showUsernames}>
+									<p class="mt-2">
+										Owner:{' '}
+										<a
+											href={`${routes.USERS}/${card.username}`}
+											class="inline font-bold hover:underline">
+											{card.username}
+										</a>
+									</p>
+								</Show>
+							</div>
+						)}
+					</For>
+				</Show>
+			</ul>
+		</div>
+	);
+}
+
+function sortCards(props: { cards: CardType[]; sort: SortType | (string & {}) }) {
+	const { cards, sort } = props;
+
+	switch (sort) {
+		case 'card-name-asc':
+			return cards.sort(
+				(a, b) => a.cardName.localeCompare(b.cardName) || a.totalOfType - b.totalOfType
+			);
+		case 'card-name-desc':
+			return cards.sort(
+				(a, b) => b.cardName.localeCompare(a.cardName) || a.totalOfType - b.totalOfType
+			);
+		case 'rarest':
+			return cards.sort(
+				(a, b) => a.totalOfType - b.totalOfType || a.cardName.localeCompare(b.cardName)
+			);
+		case 'common':
+			return cards.sort(
+				(a, b) => b.totalOfType - a.totalOfType || a.cardName.localeCompare(b.cardName)
+			);
+		default:
+			return cards;
+	}
+}
