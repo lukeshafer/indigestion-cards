e6e70157974c8fc330d22bbc5100f8450db86484
Luke Shafer <lukealanshafer@gmail.com>
2023-08-23 13:24:42 -0400

    Remove old secrets

--- stacks/config.ts
+++ stacks/config.ts
@@ -4,16 +4,10 @@ import { Config } from 'sst/constructs';
 import { getDomainName } from './constants';
 
 export function ConfigStack({ stack }: StackContext) {
-	const streamerAccessToken = new Secret(stack, 'StreamerAccessToken');
-	const streamerRefreshToken = new Secret(stack, 'StreamerRefreshToken');
-	const appAccessToken = new Secret(stack, 'AppAccessToken');
-	const twitchTokens = new Secret(stack, 'TwitchTokens', {
-		secretObjectValue: {
-			app_access_token: appAccessToken.secretValue,
-			streamer_access_token: streamerAccessToken.secretValue,
-			streamer_refresh_token: streamerRefreshToken.secretValue,
-		},
-	});
+	//const streamerAccessToken = new Secret(stack, 'StreamerAccessToken');
+	//const streamerRefreshToken = new Secret(stack, 'StreamerRefreshToken');
+	//const appAccessToken = new Secret(stack, 'AppAccessToken');
+	const twitchTokens = new Secret(stack, 'TwitchTokens');
 
 	const params = Config.Parameter.create(stack, {
 		//STREAMER_ACCESS_TOKEN_ARN: streamerAccessToken.secretArn,
