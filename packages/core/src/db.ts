4060601a17b517e4a3d78208323b87d8a2e8493e
Luke Shafer <lukealanshafer@gmail.com>
2023-10-08 17:04:26 -0400

    Add initial trades entity

--- packages/core/src/db.ts
+++ packages/core/src/db.ts
@@ -1067,6 +1067,131 @@ const siteConfig = new Entity(
 	config
 );
 
+const tradeCardsProperties = {
+	instanceId: {
+		type: 'string',
+		required: true,
+	},
+	designId: {
+		type: 'string',
+		required: true,
+	},
+	cardName: {
+		type: 'string',
+		required: true,
+	},
+	cardDescription: {
+		type: 'string',
+		required: true,
+	},
+	imgUrl: {
+		type: 'string',
+		required: true,
+	},
+	rarityId: {
+		type: 'string',
+		required: true,
+	},
+	rarityName: {
+		type: 'string',
+		required: true,
+	},
+	rarityColor: {
+		type: 'string',
+		required: true,
+	},
+	frameUrl: {
+		type: 'string',
+		required: true,
+	},
+	cardNumber: {
+		type: 'number',
+		required: true,
+	},
+	totalOfType: {
+		type: 'number',
+		required: true,
+	},
+} as const;
+
+const trades = new Entity(
+	{
+		model: {
+			entity: 'trade',
+			version: '1',
+			service: 'card-app',
+		},
+		attributes: {
+			tradeId: {
+				type: 'string',
+				required: true,
+			},
+			senderUserId: {
+				type: 'string',
+				required: true,
+			},
+			senderUsername: {
+				type: 'string',
+				required: true,
+			},
+			receiverUserId: {
+				type: 'string',
+				required: true,
+			},
+			receiverUsername: {
+				type: 'string',
+				required: true,
+			},
+			offeredCards: {
+				type: 'list',
+				required: true,
+				items: {
+					type: 'map',
+					properties: tradeCardsProperties,
+				},
+			},
+			receivedCards: {
+				type: 'list',
+				required: true,
+				items: {
+					type: 'map',
+					properties: tradeCardsProperties,
+				},
+			},
+			status: {
+				type: ['pending', 'accepted', 'rejected'] as const,
+				required: true,
+				default: 'pending',
+			},
+			...auditAttributes('trade'),
+		},
+		indexes: {
+			bySenderId: {
+				pk: {
+					field: 'pk',
+					composite: ['senderUserId'],
+				},
+				sk: {
+					field: 'sk',
+					composite: ['tradeId'],
+				},
+			},
+			byReceiverId: {
+				index: 'gsi1',
+				pk: {
+					field: 'gsi1pk',
+					composite: ['receiverUserId'],
+				},
+				sk: {
+					field: 'gsi1sk',
+					composite: ['tradeId'],
+				},
+			},
+		},
+	},
+	config
+);
+
 export const db = new Service(
 	{
 		cardDesigns,
@@ -1082,6 +1207,7 @@ export const db = new Service(
 		twitchEventMessageHistory,
 		siteConfig,
 		userLogins,
+		trades,
 	},
 	config
 );
