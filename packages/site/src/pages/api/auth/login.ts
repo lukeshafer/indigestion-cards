ada502154d80401c650d60ea937d3448de43045e
Luke Shafer <lukealanshafer@gmail.com>
2023-10-31 13:48:29 -0400

    Update auth redirect

--- packages/site/src/pages/api/auth/login.ts
+++ packages/site/src/pages/api/auth/login.ts
@@ -1,18 +1,18 @@
 import { Auth } from 'sst/node/future/auth';
 import type { APIRoute } from 'astro';
 import { authApi } from '@/constants';
-import { AstroSite } from "sst/node/site"
 import { Config } from 'sst/node/config';
 
 export const GET: APIRoute = async (ctx) => {
 	const client_id = ctx.url.host === 'localhost:4321' ? 'local' : 'main';
 	const isAdmin = ctx.locals.session?.type === 'admin';
 
-	console.log("redirect_uri: ", 'https://' + Config.DOMAIN_NAME + authApi.CALLBACK, { AstroSiteUrl: AstroSite.site.url, origin: ctx.url.origin, ConfigUrl: Config.DOMAIN_NAME })
+	console.log("redirect_uri: ", 'https://' + Config.DOMAIN_NAME + authApi.CALLBACK, { origin: ctx.url.origin, ConfigUrl: Config.DOMAIN_NAME })
+	const redirect_uri = client_id === 'local' ? ctx.url.origin + authApi.CALLBACK : 'https://' + Config.DOMAIN_NAME + authApi.CALLBACK;
 
 	const authParams = new URLSearchParams({
-		client_id: client_id,
-		redirect_uri: ctx.url.origin + authApi.CALLBACK,
+		client_id,
+		redirect_uri,
 		response_type: 'code',
 		provider: isAdmin ? 'twitchStreamer' : 'twitchUser',
 	}).toString();
