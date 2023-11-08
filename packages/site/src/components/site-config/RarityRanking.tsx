baa6f279aff786f63e9f5785e8d5454d12872875
Luke Shafer <lukealanshafer@gmail.com>
2023-11-08 17:19:02 -0500

    Add drag and drop to rarity ranking

--- packages/site/src/components/site-config/RarityRanking.tsx
+++ packages/site/src/components/site-config/RarityRanking.tsx
@@ -1,5 +1,5 @@
 import type { SiteConfig } from '@lil-indigestion-cards/core/db/siteConfig';
-import { For } from 'solid-js';
+import { For, createSignal, createUniqueId } from 'solid-js';
 import { FULL_ART_ID, LEGACY_CARD_ID } from '@/constants';
 import { createStore, produce } from 'solid-js/store';
 import { useViewTransition } from '@/lib/client/utils';
@@ -38,31 +38,17 @@ export default function RarityRanking(props: {
 		}),
 	});
 
-	const moveUp = (index: number) => {
-		if (index <= 0) return;
-		props.setIsEdited();
+	const moveToIndex = (fromIndex: number, toIndex: number) => {
+		if (fromIndex === toIndex) return;
+		if (toIndex < 0 || toIndex >= state.ranking.length) return;
 
 		useViewTransition(() => {
+			props.setIsEdited();
 			setState(
 				'ranking',
 				produce((draft) => {
-					const moving = draft.splice(index, 1)[0];
-					draft.splice(index - 1, 0, moving);
-				})
-			);
-		});
-	};
-
-	const moveDown = (index: number) => {
-		if (index >= state.ranking.length) return;
-		props.setIsEdited();
-
-		useViewTransition(() => {
-			setState(
-				'ranking',
-				produce((draft) => {
-					const moving = draft.splice(index, 1)[0];
-					draft.splice(index + 1, 0, moving);
+					const item = draft.splice(fromIndex, 1)[0];
+					draft.splice(toIndex, 0, item);
 				})
 			);
 		});
@@ -75,41 +61,103 @@ export default function RarityRanking(props: {
 			<ul class="w-full">
 				<For each={state.ranking}>
 					{(rarity, index) => (
-						<li
-							class="even:bg-brand-100 dark:even:bg-brand-dark/50 group flex 
-						items-center odd:bg-gray-300 dark:odd:bg-gray-800"
-							style={{
-								'view-transition-name': rarity.rarityId,
-							}}>
-							<span class="flex flex-col px-1">
-								<button
-									name="up"
-									classList={{ 'opacity-20': index() === 0 }}
-									onClick={(e) => {
-										e.preventDefault();
-										moveUp(index());
-									}}>
-									<Arrow up />
-								</button>
-								<button
-									name="down"
-									classList={{
-										'opacity-20': index() === state.ranking.length - 1,
-									}}
-									onClick={(e) => {
-										e.preventDefault();
-										moveDown(index());
-									}}>
-									<Arrow down />
-								</button>
-							</span>
-							<span class="inline-block w-8 p-2 text-center">{index() + 1}</span>
-							<span>{rarity.rarityName}</span>
-						</li>
+						<RankingItem
+							rarity={rarity}
+							index={index()}
+							moveToIndex={moveToIndex}
+							listLength={state.ranking.length}
+						/>
 					)}
 				</For>
 			</ul>
 		</section>
+	);
+}
+
+function RankingItem(props: {
+	rarity: Rarity;
+	index: number;
+	moveToIndex(fromIndex: number, toIndex: number): void;
+	listLength: number;
+}) {
+	const [isDragging, setIsDragging] = createSignal(false);
+	const [isDragOver, setIsDragOver] = createSignal(false);
+
+	return (
+		<li
+			draggable="true"
+			id={createUniqueId()}
+			onDragStart={(e) => {
+				const index = props.index;
+				setIsDragging(true);
+				if (!e.dataTransfer) return;
+				e.dataTransfer.setData('text', String(index));
+				e.dataTransfer.effectAllowed = 'move';
+			}}
+			onDragEnd={() => setIsDragging(false)}
+			onDragEnter={(e) => {
+				e.preventDefault();
+				setIsDragOver(true);
+			}}
+			onDragOver={(e) => e.preventDefault()}
+			onDragLeave={() => setIsDragOver(false)}
+			onDrop={(e) => {
+				e.preventDefault();
+				setIsDragOver(false);
+				const fromIndexString = e.dataTransfer?.getData('text');
+				const fromIndex = fromIndexString ? parseInt(fromIndexString) : null;
+				if (fromIndex === null) return;
+				props.moveToIndex(fromIndex, props.index);
+			}}
+			class="even:bg-brand-100 dark:even:bg-brand-dark/50 group flex 
+						items-center odd:bg-gray-300 dark:odd:bg-gray-800"
+			classList={{
+				'opacity-50': isDragOver() || isDragging(),
+			}}
+			style={{ 'view-transition-name': props.rarity.rarityId }}>
+			<span class="flex flex-col px-1">
+				<button
+					name="up"
+					classList={{ 'opacity-20': props.index === 0 }}
+					onClick={(e) => {
+						e.preventDefault();
+						props.moveToIndex(props.index, props.index - 1);
+					}}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="18"
+						height="18"
+						viewBox="0 0 24 24">
+						<path
+							fill="currentColor"
+							d="M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19z"
+						/>
+					</svg>
+				</button>
+				<button
+					name="down"
+					classList={{
+						'opacity-20': props.index === props.listLength - 1,
+					}}
+					onClick={(e) => {
+						e.preventDefault();
+						props.moveToIndex(props.index, props.index + 1);
+					}}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="18"
+						height="18"
+						viewBox="0 0 24 24">
+						<path
+							fill="currentColor"
+							d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"
+						/>
+					</svg>
+				</button>
+			</span>
+			<span class="inline-block w-8 p-2 text-center">{props.index + 1}</span>
+			<span>{props.rarity.rarityName}</span>
+		</li>
 	);
 }
 
@@ -118,8 +166,8 @@ function Arrow(props: { up?: boolean; down?: boolean }) {
 		props.up
 			? 'M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19z'
 			: props.down
-				? 'M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z'
-				: '';
+			? 'M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z'
+			: '';
 
 	return (
 		<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
--- packages/site/src/components/site-config/SiteConfigForm.tsx
+++ packages/site/src/components/site-config/SiteConfigForm.tsx
@@ -6,6 +6,7 @@ import { API } from '@/constants';
 import { Show, createSignal } from 'solid-js';
 import Table from '../table/Table';
 import RarityRanking, { type RarityRankingRecord } from './RarityRanking';
+import { PageHeader, PageTitle } from '../text';
 
 export default function SiteConfigForm(props: {
 	baseRarityValue: string;
@@ -20,85 +21,94 @@ export default function SiteConfigForm(props: {
 	const [rarity, setRarity] = createSignal(props.baseRarityValue);
 
 	return (
-		<Form action={API.SITE_CONFIG} method="post" onsuccess={() => setIsEdited(false)}>
-			<Show when={isEdited()}>
-				<SubmitButton>Save</SubmitButton>
-			</Show>
-			<Select
-				name="base-rarity"
-				label="Base Rarity for Design Previews"
-				value={rarity()}
-				setValue={(val) => {
-					setIsEdited(true);
-					setRarity(val);
-				}}
-				options={props.rarities.map((rarity) => {
-					const value = new URLSearchParams({
-						rarityId: rarity.rarityId,
-						frameUrl: rarity.frameUrl,
-						rarityColor: rarity.rarityColor,
-						rarityName: rarity.rarityName,
-					}).toString();
-					return { value, label: rarity.rarityName };
-				})}
-			/>
-			<RarityRanking
-				rarities={props.rarities}
-				initialRanking={props.initialRanking}
-				setIsEdited={() => setIsEdited(true)}
-			/>
-			<Table
-				search={{
-					label: 'Search Events',
-					column: 'event',
-				}}
-				columns={[
-					{
-						name: 'event',
-						label: 'Event',
-						font: 'title',
-						width: '66%',
-					},
-					{
-						name: 'packType',
-						label: 'Pack Type',
-						sort: false,
-					},
-				]}
-				rows={[
-					{
-						event: '5 Gift Subs',
-						packType: {
-							element: (
-								<TwitchEventSelect
-									twitchEvent={props.giftSubEvent}
-									packTypes={props.packTypes}
-									onChange={() => {
-										setIsEdited(true);
-									}}
-								/>
-							),
-							value: '',
+		<>
+			<Form action={API.SITE_CONFIG} method="post" onsuccess={() => setIsEdited(false)}>
+				<div class="mx-auto">
+					<PageHeader>
+						<Show when={isEdited()}>
+							<SubmitButton transitionId="site-config-save-btn">Save</SubmitButton>
+						</Show>
+						<PageTitle style={{ 'view-transition-name': 'site-config-title' }}>
+							Site Config
+						</PageTitle>
+					</PageHeader>
+				</div>
+				<Select
+					name="base-rarity"
+					label="Base Rarity for Design Previews"
+					value={rarity()}
+					setValue={(val) => {
+						setIsEdited(true);
+						setRarity(val);
+					}}
+					options={props.rarities.map((rarity) => {
+						const value = new URLSearchParams({
+							rarityId: rarity.rarityId,
+							frameUrl: rarity.frameUrl,
+							rarityColor: rarity.rarityColor,
+							rarityName: rarity.rarityName,
+						}).toString();
+						return { value, label: rarity.rarityName };
+					})}
+				/>
+				<RarityRanking
+					rarities={props.rarities}
+					initialRanking={props.initialRanking}
+					setIsEdited={() => setIsEdited(true)}
+				/>
+				<Table
+					search={{
+						label: 'Search Events',
+						column: 'event',
+					}}
+					columns={[
+						{
+							name: 'event',
+							label: 'Event',
+							font: 'title',
+							width: '66%',
 						},
-					},
-					...props.twitchEvents.map((event) => ({
-						event: `Reward: ${event.eventName}`,
-						packType: {
-							element: (
-								<TwitchEventSelect
-									twitchEvent={event}
-									packTypes={props.packTypes}
-									onChange={() => {
-										setIsEdited(true);
-									}}
-								/>
-							),
-							value: '',
+						{
+							name: 'packType',
+							label: 'Pack Type',
+							sort: false,
 						},
-					})),
-				]}
-			/>
-		</Form>
+					]}
+					rows={[
+						{
+							event: '5 Gift Subs',
+							packType: {
+								element: (
+									<TwitchEventSelect
+										twitchEvent={props.giftSubEvent}
+										packTypes={props.packTypes}
+										onChange={() => {
+											setIsEdited(true);
+										}}
+									/>
+								),
+								value: '',
+							},
+						},
+						...props.twitchEvents.map((event) => ({
+							event: `Reward: ${event.eventName}`,
+							packType: {
+								element: (
+									<TwitchEventSelect
+										twitchEvent={event}
+										packTypes={props.packTypes}
+										onChange={() => {
+											setIsEdited(true);
+										}}
+									/>
+								),
+								value: '',
+							},
+						})),
+					]}
+				/>
+			</Form>
+		</>
 	);
 }
 
