f9d58df228740ae4315faf06f83bdc672ed3cf9a
Luke Shafer <lukealanshafer@gmail.com>
2023-11-22 00:34:00 -0500

    Add frontend package with SolidStart scaffolding

---
+++ packages/frontend/vite.config.ts
@@ -0,0 +1,7 @@
+import solid from 'solid-start/vite';
+import aws from 'solid-start-sst';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [solid({ adapter: aws() })],
+});
