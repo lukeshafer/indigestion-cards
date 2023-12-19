89a2b7cb3b327c7190c8d8ee345d94082a44c3d5
Luke Shafer <lukealanshafer@gmail.com>
2023-12-19 13:51:31 -0500

    Update admin auth callback for better logging

--- packages/admin/src/pages/api/auth/callback.ts
+++ packages/admin/src/pages/api/auth/callback.ts
@@ -13,7 +13,7 @@ export async function GET(ctx: APIContext) {
 	const client_id = ctx.url.host === 'localhost:4321' ? 'local' : 'main';
 	const origin = client_id === 'local' ? ctx.url.origin : 'https://' + Config.DOMAIN_NAME;
 
-	const response = await fetch(Auth.AdminSiteAuth.url + '/token', {
+	const bodyText = await fetch(Auth.AdminSiteAuth.url + '/token', {
 		method: 'POST',
 		body: new URLSearchParams({
 			grant_type: 'authorization_code',
@@ -21,27 +21,28 @@ export async function GET(ctx: APIContext) {
 			code,
 			redirect_uri: `${origin}${ctx.url.pathname}`,
 		}),
-	})
-		.then((r) => r.text())
-		.then((r) => {
-			//console.log(r);
-			return JSON.parse(r)
-		}).catch((err) => {
-			console.error('An error occurred parsing the JSON.', { err })
-		});
+	}).then((r) => r.text());
 
-	if (!response.access_token) {
+	const body = ((text: string) => {
+		try {
+			return JSON.parse(text);
+		} catch (err) {
+			console.error('An error occurred parsing the JSON.', { err, text });
+		}
+	})(bodyText);
+
+	if (!body.access_token) {
 		throw new Error('No access token');
 	}
 
-	ctx.cookies.set(AUTH_TOKEN, response.access_token, {
+	ctx.cookies.set(AUTH_TOKEN, body.access_token, {
 		maxAge: 60 * 60 * 24 * 30,
 		httpOnly: true,
 		secure: ctx.url.host !== 'localhost',
 		path: '/',
 	});
 
-	const session = Session.verify(response.access_token);
+	const session = Session.verify(body.access_token);
 
 	if (session.type === 'admin' || session.type === 'user') {
 		return ctx.redirect(`/?alert=Logged in!`, 302);
