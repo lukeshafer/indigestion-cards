3d0834072475204bae48512971c9b1d7e96fe99a
Luke Shafer <lukealanshafer@gmail.com>
2023-12-30 15:30:30 -0500

    Improve styles on RemainingPackCount

--- packages/site/src/pages/data/packs-remaining.ts
+++ packages/site/src/pages/data/packs-remaining.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from 'astro';
-import { TypedResponse } from '@/lib/api';
+import { TypedResponse, cacheControl, time } from '@/lib/api';
 import { getAllCardDesigns, getCardDesignAndInstancesById } from '@lib/design'
 import type { CardDesign } from '@lil-indigestion-cards/core/db/cardDesigns';
 
@@ -9,7 +9,15 @@ export const GET = (async () => {
   const designCountData = await Promise.all(designs.map(getDesignCountData))
   const counts = [...designCountData.reduce(reducer, new Map()).values()]
 
-  return new TypedResponse(counts);
+  return new TypedResponse(counts, {
+    headers: {
+      'cache-control': cacheControl({
+        public: true,
+        maxAge: 0,
+        staleWhileRevalidate: time({minutes: 1})
+      })
+    }
+  });
 }) satisfies APIRoute;
 
 export interface DesignCountData {
