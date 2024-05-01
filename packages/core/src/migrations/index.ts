dd446f952a3d39ebf1e59c48d4bffe55670a5f35
Luke Shafer <lukealanshafer@gmail.com>
2024-05-01 02:03:58 -0400

    Add full refator migration script

--- packages/core/src/migrations/index.ts
+++ packages/core/src/migrations/index.ts
@@ -1,21 +1,21 @@
 import { migration as secretsToSSM } from './secrets-to-ssm';
 
-import {
-	packs1To2,
-	instances1To2,
-	cardTradeHistoryFlipToFrom,
-	designs1To2,
-	addBestRarityFound,
-} from './db-old-migrations';
+// import {
+// 	packs1To2,
+// 	instances1To2,
+// 	cardTradeHistoryFlipToFrom,
+// 	designs1To2,
+// 	addBestRarityFound,
+// } from './db-old-migrations';
 
 // TODO: remove legacy migrations, once final migration to new db is complete
 export async function migration() {
 	await secretsToSSM();
 
   // LEGACY
-	await packs1To2();
-	await addBestRarityFound();
-	await designs1To2();
-	await cardTradeHistoryFlipToFrom();
-	await instances1To2();
+	// await packs1To2();
+	// await addBestRarityFound();
+	// await designs1To2();
+	// await cardTradeHistoryFlipToFrom();
+	// await instances1To2();
 }
--- stacks/script.ts
+++ stacks/script.ts
@@ -3,27 +3,34 @@ import { Database } from './database';
 import { ConfigStack } from './config';
 
 export function AfterDeployStack({ stack }: StackContext) {
-  const db = use(Database);
-  const config = use(ConfigStack);
+	const db = use(Database);
+	const config = use(ConfigStack);
 
-  const script = new Script(stack, 'AfterDeployScript', {
-    onCreate: 'packages/functions/src/deployment/seed-db.handler',
-    onDelete: 'packages/functions/src/deployment/delete-twitch-events.handler',
-    onUpdate: 'packages/functions/src/deployment/migrate-db.handler',
-    defaults: {
-      function: {
-        bind: [
-          db,
-          config.TWITCH_CLIENT_ID,
-          config.TWITCH_CLIENT_SECRET,
-          config.TWITCH_TOKENS_PARAM,
-          config.STREAMER_USER_ID,
-        ],
-        permissions: ['ssm:GetParameter', 'ssm:PutParameter'],
-        runtime: 'nodejs18.x',
-      },
-    },
-  });
+	new Script(stack, 'RefactorFn', {
+		onCreate: {
+			handler: 'packages/functions/src/deployment/mega-refactor-migration.handler',
+			bind: [db],
+		},
+	});
 
-  return script;
+	const script = new Script(stack, 'AfterDeployScript', {
+		onCreate: 'packages/functions/src/deployment/seed-db.handler',
+		onDelete: 'packages/functions/src/deployment/delete-twitch-events.handler',
+		onUpdate: 'packages/functions/src/deployment/migrate-db.handler',
+		defaults: {
+			function: {
+				bind: [
+					db,
+					config.TWITCH_CLIENT_ID,
+					config.TWITCH_CLIENT_SECRET,
+					config.TWITCH_TOKENS_PARAM,
+					config.STREAMER_USER_ID,
+				],
+				permissions: ['ssm:GetParameter', 'ssm:PutParameter'],
+				runtime: 'nodejs18.x',
+			},
+		},
+	});
+
+	return script;
 }
