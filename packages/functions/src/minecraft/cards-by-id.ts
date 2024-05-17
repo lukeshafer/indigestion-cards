1c5e56d6078252a3655f3c8c906e1de24d47a1e1
Luke <lukealanshafer@gmail.com>
2024-05-16 22:31:38 -0400

    Add minecraft card image logic

---
+++ packages/functions/src/minecraft/cards-by-id.ts
@@ -0,0 +1,108 @@
+import { getCardInstanceByUsername } from '@core/lib/card';
+import type { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+import sharp from 'sharp';
+import type { CardInstance } from '../../../core/src/db.types';
+
+export async function handler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
+	const { instanceId, username } = event.pathParameters ?? {};
+
+	let missingParameters = [];
+	if (!instanceId) {
+		missingParameters.push('instanceId');
+	}
+	if (!username) {
+		missingParameters.push('username');
+	}
+
+	if (!instanceId || !username) {
+		return {
+			statusCode: 400,
+			body: `Missing required parameters: ${missingParameters.join()}`,
+		};
+	}
+
+	let card = await getCardInstanceByUsername({ instanceId, username });
+
+	if (!card) {
+		return {
+			statusCode: 404,
+			body: `Card not found with instanceId ${instanceId} for username ${username}`,
+		};
+	}
+
+	let cardImageBuffer;
+	try {
+		cardImageBuffer = await getImage(card);
+	} catch {
+		return {
+			body: 'Fetch failed',
+			statusCode: 500,
+		};
+	}
+
+	let filePathPrefix;
+	if (process.env.IS_LOCAL === 'true') {
+		filePathPrefix = './packages/functions/src/minecraft/fonts';
+	} else {
+		filePathPrefix = './fonts';
+	}
+
+	const img = await sharp(cardImageBuffer)
+		.resize({
+			width: 128,
+			kernel: sharp.kernel.mitchell,
+		})
+		.composite([
+			{
+				input: {
+					text: {
+						text: card.cardName,
+						rgba: true,
+						fontfile: filePathPrefix + '/minecraft.ttf',
+						dpi: 49,
+					},
+				},
+				left: 16,
+				top: 9,
+			},
+			{
+				input: {
+					text: {
+						text: card.cardDescription,
+						rgba: true,
+						fontfile: filePathPrefix + '/minecraft.ttf',
+						dpi: 32,
+            width: 95
+					},
+				},
+				left: 17,
+				top: 126,
+			},
+		])
+		.png()
+		.toBuffer();
+
+	// Convert said card into an image in minecraft style
+	return {
+		body: img.toString('base64'),
+		isBase64Encoded: true,
+		statusCode: 200,
+		headers: {
+			'Content-Type': 'image/png',
+			'Content-Length': String(img.length),
+		},
+	};
+}
+
+async function getImage(card: CardInstance) {
+	const url = new URL(`https://${process.env.CARD_CDN_URL!}`);
+	url.pathname = `/${card.designId}/${card.rarityId}.png`;
+	// console.log(url);
+
+	const response = await fetch(url);
+	// console.log({ response });
+	const buffer = await response.arrayBuffer();
+	// console.log({ body });
+
+	return buffer;
+}
--- stacks/minecraft.ts
+++ stacks/minecraft.ts
@@ -2,20 +2,32 @@ import { StackContext, Api, use } from 'sst/constructs';
 import { Database } from './database';
 import { DesignBucket } from './bucket';
 import { getHostedZone, getDomainName } from './constants';
+import { ImageProcessing } from './image-processing';
 
 export function Minecraft({ app, stack }: StackContext) {
 	const table = use(Database);
 	const { frameBucket, cardDesignBucket } = use(DesignBucket);
+	const { cardCDN } = use(ImageProcessing);
 
 	const hostedZone = getHostedZone(app.stage);
 	const baseDomain = getDomainName(app.stage);
 
 	const minecraftApi = new Api(stack, 'MinecraftApi', {
 		routes: {
-			'GET /cards/by-id': 'packages/functions/src/minecraft/cards-by-id.handler',
+			'GET /card/{username}/{instanceId}/card.png':
+				'packages/functions/src/minecraft/cards-by-id.handler',
 		},
 		defaults: {
 			function: {
+				copyFiles: [
+					{
+						from: 'packages/functions/src/minecraft/fonts',
+						to: 'fonts',
+					},
+				],
+				environment: {
+					CARD_CDN_URL: cardCDN.domainName,
+				},
 				bind: [table, frameBucket, cardDesignBucket],
 				runtime: 'nodejs18.x',
 			},
