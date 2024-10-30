fbb13ee65b719681af4719d063d67fc2c3a771c2
Luke <lukealanshafer@gmail.com>
2024-10-30 00:37:07 -0400

    Fix view transitions showing when animations are disabled. Load pack count on SSR. Add some style to site logo

--- packages/site/src/components/pack/TotalPackCount.tsx
+++ packages/site/src/components/pack/TotalPackCount.tsx
@@ -2,7 +2,7 @@ import { Show, createResource } from 'solid-js';
 import ButtonCount from '../ButtonCount';
 import { get } from '@site/lib/client/data';
 
-export default function TotalPackCount() {
+export default function TotalPackCount(props: { initialCount: number }) {
 	const [count] = createResource(async () => get('pack-count').then(d => d.packCount));
 	return <Show when={count()}>{packCount => <ButtonCount>{packCount()}</ButtonCount>}</Show>;
 }
