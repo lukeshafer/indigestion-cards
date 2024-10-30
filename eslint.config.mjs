5e9a85f3a23e8c44738451635f20df454afaa5b5
Luke <lukealanshafer@gmail.com>
2024-10-30 19:55:26 -0400

    Fix eslint config, change serverroutes for admin site

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,38 @@
-export default {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	ignorePatterns: ['dist/', 'node_modules/'],
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:solid/typescript',
-		'plugin:astro/recommended',
-		'plugin:astro/jsx-a11y-recommended',
-	],
-	overrides: [
-		{
-			files: ['.eslintrc.{js,cjs}'],
-			env: {
-				node: true,
-			},
-			parserOptions: {
-				sourceType: 'script',
-			},
-		},
-		{
-			files: ['*.astro'],
-			processor: 'astro/client-side-ts',
-			parser: 'astro-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser',
-				extraFileExtensions: ['.astro'],
-			},
-		},
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['@typescript-eslint', 'solid'],
-	rules: {
-		'prefer-const': 'off',
-		'no-mixed-spaces-and-tabs': 'off',
-		'solid/style-prop': 'off',
-		'solid/self-closing-comp': 'off',
-		'no-mixed-spaces-and-tabs': 'off',
-		'@typescript-eslint/triple-slash-reference': 'off',
-		'@typescript-eslint/no-namespace': 'off',
-	},
-};
+//@ts-check
+import js from '@eslint/js';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+import solid from 'eslint-plugin-solid/configs/typescript';
+import astro from 'eslint-plugin-astro';
+//import jsxA11y from 'eslint-plugin-jsx-a11y'
+
+export default ts.config(
+  js.configs.recommended,
+  //jsxA11y.flatConfigs.recommended,
+  solid,
+  ...ts.configs.recommended,
+  ...astro.configs['flat/jsx-a11y-recommended'],
+  ...astro.configs['flat/recommended'],
+  {
+    ignores: ['**/dist/', '**/node_modules/'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    rules: {
+      'prefer-const': 'off',
+      'no-mixed-spaces-and-tabs': 'off',
+      'solid/style-prop': 'off',
+      'solid/self-closing-comp': 'off',
+      '@typescript-eslint/triple-slash-reference': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+    },
+  },
+);
